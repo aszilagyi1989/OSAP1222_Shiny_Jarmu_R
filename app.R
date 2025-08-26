58014562753eb6cdbd5fb3ced5b9dbcff1b24093
vl05953 <adam.szilagyi@ksh.hu>
2025-08-26 12:23:15 +0200

    Repair GWalkR

--- app.R
+++ app.R
@@ -18,6 +18,7 @@ library("ggplot2")
 library("plotly")
 library("GWalkR")
 library("TSstudio")
+library("plotrix")
 
 DATA <- read.csv("https://raw.githubusercontent.com/aszilagyi1989/Shiny_CSV/refs/heads/main/OSAP1222_Jarmu.csv", sep = ";", row.names = NULL)
 DATA$MHO <- str_pad(DATA$MHO, width = 2, pad = "0")
@@ -51,12 +52,12 @@ ui <- page_sidebar(
     ),
     uiOutput("bordercity"),
     uiOutput("search"),
-    selectInput("diagram", "Diagramtípus", c("Vonal", "Pont", "Kör", "Idősor", "Dekompozíciós modell"), selected = "Vonal"),
+    selectInput("diagram", "Diagramtípus", c("Vonal", "Pont", "Kör", "Doboz", "Idősor", "Dekompozíciós modell"), selected = "Vonal"), # , "3D Kör"
     selectInput("forgalom", "Dekompozíció iránya", c("Belépő", "Kilépő"), selected = "Belépő")
   ),
   navset_card_underline(id = "navset", 
                         nav_panel("Vonal- vagy pontdiagram", plotlyOutput("linechart")),
-                        nav_panel("Tableau", gwalkrOutput("tableau")),
+                        nav_panel("Tableau", uiOutput("tableau")),
                         nav_spacer(),
                         nav_menu(title = "Hivatkozások", align = "right", 
                                  nav_item(tags$a("Személyforgalom", href = "https://aszilagyi.shinyapps.io/OSAP1222_Shiny_Szemely_R/", target = "_blank")), 
@@ -103,19 +104,30 @@ server <- function(input, output, session) {
   
   
   observeEvent(input$navset, {
+    
     if(input$navset == "Tableau") {
       
-      shinyjs::disable("date")
-      shinyjs::disable("bordercity")
-      shinyjs::disable("search")
       shinyjs::disable("diagram")
       shinyjs::disable("forgalom")
       
+      subsetted <- reactive({
+        
+        DATA %>% select(-c(TEV, MHO)) %>% arrange(DATE, MG05, MG58, MG64, MG60) -> DATA
+        DATA %>% filter(DATE >= input$date[[1]] & DATE <= input$date[[2]] & MG64 %in% input$search & MG05 %in% input$bordercity)
+        
+      })
+      
+      output$tableau <- renderUI({
+        
+        subsetted <- subsetted()
+        colnames(subsetted) <- c("Irány", "Honosság", "Határátkelőhely", "Jármű", "Forgalom", "Dátum")
+        
+        renderGwalkr({ gwalkr(subsetted) })
+        
+      })
+      
     } else {
       
-      shinyjs::enable("date")
-      shinyjs::enable("bordercity")
-      shinyjs::enable("search")
       shinyjs::enable("diagram")
       if (input$diagram == "Dekompozíciós modell")
         shinyjs::enable("forgalom")
@@ -146,7 +158,7 @@ server <- function(input, output, session) {
     
     DATA %>% select(-c(TEV, MHO)) %>% arrange(DATE, MG05, MG58, MG64, MG60) -> DATA
     
-    if(input$navset != "Tableau" && (input$diagram == "Vonal" || input$diagram == "Pont" || input$diagram == "Kör")){
+    if(input$navset != "Tableau" && (input$diagram == "Vonal" || input$diagram == "Pont" || input$diagram == "Kör" || input$diagram == "3D Kör" || input$diagram == "Doboz")){
       
       DATA %>% filter(DATE >= input$date[[1]] & DATE <= input$date[[2]] & MG64 %in% input$search & MG05 %in% input$bordercity)
       
@@ -181,13 +193,13 @@ server <- function(input, output, session) {
         if(input$diagram == "Vonal"){
           
           colnames(subsetted) <- c("Irány", "Honosság", "Határátkelőhely", "Jármű", "Forgalom", "Dátum")
-          map <- ggplot(data = subsetted, aes(x = Dátum, y = Forgalom, colour = Honosság)) + geom_line() + labs(x = "Dátum", y = "Forgalom", colour = "Honosság") + facet_wrap(facet = vars(subsetted$Irány, subsetted$Jármű))
+          map <- ggplot(data = subsetted, aes(x = Dátum, y = Forgalom, colour = Honosság, fill = Honosság)) + geom_line() + labs(x = "Dátum", y = "Forgalom", colour = "Honosság") + facet_wrap(facet = vars(subsetted$Irány, subsetted$Jármű), nrow = 2)
           ggplotly(map)
           
         }else if (input$diagram == "Pont"){
           
           colnames(subsetted) <- c("Irány", "Honosság", "Határátkelőhely", "Jármű", "Forgalom", "Dátum")
-          map <- ggplot(data = subsetted, aes(x = Dátum, y = Forgalom, fill = Honosság)) + geom_point() + facet_wrap(facet = vars(subsetted$Irány, subsetted$Jármű))
+          map <- ggplot(data = subsetted, aes(x = Dátum, y = Forgalom, colour = Honosság, fill = Honosság)) + geom_point() + facet_wrap(facet = vars(subsetted$Irány, subsetted$Jármű), nrow = 2)
           ggplotly(map)
           
         }else if (input$diagram == "Kör"){
@@ -195,10 +207,24 @@ server <- function(input, output, session) {
           colnames(subsetted) <- c("Irány", "Honosság", "Határátkelőhely", "Jármű", "Forgalom", "Dátum")
           pie1 <- plot_ly(subsetted, labels = subsetted[subsetted$Irány == "Belépő", "Jármű"], values = subsetted[subsetted$Irány == "Belépő", "Forgalom"], type = 'pie', title = "Belépő",  domain = list(x = c(0, 0.4), y = c(0.4, 1)))
           pie1 <- pie1 %>% layout(xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE), yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
-          pie2 <- plot_ly(subsetted, labels = subsetted[subsetted$Irány == "Kilépő", "Jármű"], values = subsetted[subsetted$Irány == "Kilépő", "Forgalom"], type = 'pie', title = "Kilépő",  domain = list(x = c(0.6, 1), y = c(0.4, 1)))
+          pie2 <- plot_ly(subsetted, labels = subsetted[subsetted$Irány == "Kilépő", "Jármű"], values = subsetted[subsetted$Irány == "Kilépő", "Forgalom"], type = 'pie', title = "Kilépő", domain = list(x = c(0.6, 1), y = c(0.4, 1)))
           pie2 <- pie2 %>% layout(xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE), yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
           subplot(pie1, pie2, nrows = 2, shareX = TRUE, shareY = FALSE)
           
+          
+        }else if (input$diagram == "3D Kör"){
+          
+          colnames(subsetted) <- c("Irány", "Honosság", "Határátkelőhely", "Jármű", "Forgalom", "Dátum")
+          pie3D <- pie3D(subsetted[subsetted$Irány == "Belépő", "Forgalom"], labels = subsetted[subsetted$Irány == "Belépő", "Jármű"], explode = 0.1, main = "Belépő")
+          pie3D
+          
+          
+        }else if (input$diagram == "Doboz"){
+          
+          colnames(subsetted) <- c("Irány", "Honosság", "Határátkelőhely", "Jármű", "Forgalom", "Dátum")
+          map <- ggplot(data = subsetted, aes(x = Jármű, y = Forgalom, fill = Irány, colour = Irány)) + geom_boxplot() + facet_wrap(facet = vars(subsetted$Honosság), nrow = 2)
+          # map <- ggplot(data = subsetted, aes(x = Jármű)) + geom_bar() # + facet_wrap(facet = vars(subsetted$Irány), nrow = 2)
+          ggplotly(map)
           
         }else if (input$diagram == "Idősor"){
           
@@ -230,20 +256,6 @@ server <- function(input, output, session) {
       })
       
     }
-    
-  })
-  
-  output$tableau <- renderGwalkr({
-    
-    req(input$date)
-    req(input$bordercity)
-    req(input$search)
-    req(input$diagram)
-    
-    subsetted <- subsetted()
-    colnames(subsetted) <- c("Irány", "Honosság", "Határátkelőhely", "Jármű", "Forgalom", "Dátum")
-    
-    gwalkr(subsetted)
     
   })
   
